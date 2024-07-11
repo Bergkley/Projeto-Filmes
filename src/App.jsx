547e9ac694e6f98672e5c11c5d2c42a011d11969
Bergkley <bergkley@gmail.com>
2024-07-10 21:30:36 -0300

    feat: criando o componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from "react";
 import './App.css'
 import { Link, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return(
     <div className="App">
-      <nav id="navbar">
-        <h2>
-        <Link to="/">MoviesLib</Link>
-        </h2>
-
-        <Link to="/movie/1">Movie</Link>
-
-        <Link to="/search">Search</Link>
-      </nav>
+      <Navbar />
       <h1>Hello World</h1>
       <Outlet />
     </div>
