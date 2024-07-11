69971311a1ed7986fa5c8d24c1d1ec427581cc1b
Bergkley <bergkley@gmail.com>
2024-07-11 19:57:21 -0300

    feat: criação do componente movieCard

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello World</h1>
       <Outlet />
     </div>
   );
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import {FaStar} from "react-icons/fa"
+const MovieCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MovieCard
