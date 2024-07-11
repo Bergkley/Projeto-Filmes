515ab8fcb672540365a39c944685de1e5d5ab2a6
Bergkley <bergkley@gmail.com>
2024-07-10 21:49:43 -0300

    feat: refatoração

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return(
+  return (
     <div className="App">
       <Navbar />
       <h1>Hello World</h1>
       <Outlet />
     </div>
-  )
+  );
 }
 export default App;
