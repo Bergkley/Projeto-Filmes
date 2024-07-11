b6e25e02766b12aa87054afec8b1505a3521b786
Bergkley <bergkley@gmail.com>
2024-07-10 21:47:54 -0300

    feat : pegando os dados da api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default Home
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topmovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
+      {topmovies.length === 0 && <p>Carregando...</p>}
+      {topmovies.length > 0 && topmovies.map((moviesURL) => <p>{moviesURL.title}</p>)}
+    </div>
+  );
+};
+export default Home;
