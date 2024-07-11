9d79e933c8dfaf58132e28c6ffb0bc5891b8206d
Bergkley <bergkley@gmail.com>
2024-07-10 21:18:50 -0300

    feat: criando os componentes da rotas

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import App from "./App";
 import './index.css'
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
