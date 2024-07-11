cf0dbd8f8e58fda891fd227a0f06ef9365591187
Bergkley <bergkley@gmail.com>
2024-07-10 21:49:31 -0300

    feat: refatoração

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
-
 import { Link } from "react-router-dom";
-import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 const Navbar = () => {
   return (
     <nav id="navbar">
-        <h2>
-        <Link to="/"><BiCameraMovie />MoviesLib</Link>
-        </h2>
-        <form action="">
-            <input type="text" placeholder="Busque um filme" />
-            <button type="submit">
-                <BiSearchAlt2 />
-            </button>
-        </form>
-      </nav>
-  )
-}
+      <h2>
+        <Link to="/">
+          <BiCameraMovie />
+          MoviesLib
+        </Link>
+      </h2>
+      <form action="">
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
