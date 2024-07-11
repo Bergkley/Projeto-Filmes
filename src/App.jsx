a7a9adcc7a5c0e7f171925205aee5508efed48fc
Bergkley <bergkley@gmail.com>
2024-07-10 21:24:52 -0300

    feat: criando a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import './App.css'
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return(
     <div className="App">
+      <nav id="navbar">
+        <h2>
+        <Link to="/">MoviesLib</Link>
+        </h2>
+
+        <Link to="/movie/1">Movie</Link>
+
+        <Link to="/search">Search</Link>
+      </nav>
       <h1>Hello World</h1>
+      <Outlet />
     </div>
   )
 }
