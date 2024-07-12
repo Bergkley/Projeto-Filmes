2edc73c486e68323d501593e6a7f1fdcf2543bc4
Bergkley <bergkley@gmail.com>
2024-07-12 17:22:01 -0300

    feat: estruturando o componente Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,6 +8,9 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 
 const Search = () => {
+  const [searchParams] = useSearchParams();
+  const [movies,setMovies] = useState([]);
+  const query = searchParams.get("q");
   return (
     <div className="container">
       <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
