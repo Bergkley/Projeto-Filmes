61a4265e6ca42ff58451afa725baf18048688b1b
Bergkley <bergkley@gmail.com>
2024-07-12 17:42:04 -0300

    feat: estruturando o componente Movie

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -7,13 +7,34 @@ import {
   BsFillFileEarmarkTextFill,
 } from "react-icons/bs";
 import "./Movie.css"
+import MovieCard from "../components/MovieCard";
 
-const searchURL = import.meta.env.VITE_SEARCH;
+
+const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-  return <div>Movie</div>;
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovie(data);
+  };
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, []);
+
+
+  return <div>
+    {movie && (
+      <>
+      {movie.title}
+      </>
+    )}
+  </div>;
 };
 
 export default Movie;
