eae584c0d2d603b42461b83821f30c14fefe726f
Bergkley <bergkley@gmail.com>
2024-07-12 17:20:28 -0300

    feat: estruturando o componente Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,7 +8,15 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="container">
+      <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  )
 };
 
 export default Search;
